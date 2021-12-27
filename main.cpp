37a812c9418b753e3d4d459417359f85a9f8ffca
mememe653 <95522491+mememe653@users.noreply.github.com>
2021-12-27 13:41:32 +1030

    Allow player cannon to move left and right

--- main.cpp
+++ main.cpp
@@ -2,9 +2,50 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
+
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
+class Player {
+private:
+	const int w = 128;
+	const int h = 128;
+	int x, y;
+	SDL_Surface* img;
+	const int moveAmount = 20;
+
+public:
+	Player() : x(SCREEN_WIDTH / 2), y(SCREEN_HEIGHT - h) {
+		img = IMG_Load("resources\\player.png");
+		if (!img) {
+			std::cout << "Failed to load resources\\player.png: " << IMG_GetError() << std::endl;
+		}
+	}
+
+	void moveRight() {
+		x += moveAmount;
+	}
+
+	void moveLeft() {
+		x -= moveAmount;
+	}
+
+	void draw(SDL_Window* window, SDL_Surface* winSurface) {
+		SDL_Rect dest;
+		dest.x = this->x;
+		dest.y = this->y;
+		SDL_BlitSurface(this->img, nullptr, winSurface, &dest);
+		SDL_UpdateWindowSurface(window);
+	}
+};
+
+
+void clearWindow(SDL_Window* window, SDL_Surface* winSurface) {
+	SDL_FillRect(winSurface, nullptr, SDL_MapRGB(winSurface->format, 0, 0, 0));
+	SDL_UpdateWindowSurface(window);
+}
+
 int main(int argc, char** args) {
-	const int SCREEN_WIDTH = 1280;
-	const int SCREEN_HEIGHT = 720;
 
 	SDL_Surface* winSurface = nullptr;
 	SDL_Window* window = nullptr;
@@ -41,17 +82,30 @@ int main(int argc, char** args) {
 	SDL_FillRect(winSurface, nullptr, SDL_MapRGB(winSurface->format, 0, 0, 0));
 	SDL_UpdateWindowSurface(window);
 
-	SDL_Surface* img = IMG_Load("resources\\player.png");
+	Player player;
+	player.draw(window, winSurface);
 
-	if (!img) {
-		std::cout << "Failed to load resources\\player.png: " << IMG_GetError() << std::endl;
+	SDL_Event ev;
+	while (true) {
+		while (SDL_PollEvent(&ev)) {
+			switch (ev.type) {
+			case SDL_KEYDOWN:
+				switch (ev.key.keysym.sym) {
+				case SDLK_LEFT:
+					player.moveLeft();
+					clearWindow(window, winSurface);
+					player.draw(window, winSurface);
+					break;
+				case SDLK_RIGHT:
+					player.moveRight();
+					clearWindow(window, winSurface);
+					player.draw(window, winSurface);
+					break;
+				}
+				break;
+			}
+		}
 	}
-
-	SDL_Rect dest;
-	dest.x = 128;
-	dest.y = 128;
-	SDL_BlitSurface(img, nullptr, winSurface, &dest);
-	SDL_UpdateWindowSurface(window);
 
 	system("pause");
 	SDL_DestroyWindow(window);
